b36d6b9b93c6602a10bc8eb5d529fee2e8fecac1
Ryan Lim <ryan.hyun@gmail.com>
2021-06-06 11:31:51 -0700

    2

---
+++ practice.py
@@ -0,0 +1,3 @@
+a = [{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"}]
+
+print(a[0]['text'])
---
+++ quiz_brain.py
@@ -0,0 +1,16 @@
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def still_has_questions(self):
+        if len(self.question_list) == self.question_number:
+            return False
+        else:
+            return True
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number].text
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question}. (True/False)?: ")
